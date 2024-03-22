ceba6a72cd60df6098eef48826b647c7daa0acbf
Yamnyr <quentinwaroquet@gmail.comm>
2024-03-22 12:09:51 +0100

    detail

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -53,7 +53,7 @@ function ResponsiveAppBar() {
                     'Authorization': token
                 },
             });
-            console.log(response)
+            // console.log(response)
             if (response.status === 401) {
                 setPages([
                     {label: 'toutes les recette', lien: '/recettes'}
@@ -91,22 +91,22 @@ function ResponsiveAppBar() {
                 <Toolbar disableGutters>
                     <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                     <Typography
-                                    variant="h6"
-                                    noWrap
-                                    component="a"
-                                    href="/"
-                                    sx={{
-                                      mr: 2,
-                                      display: { xs: 'none', md: 'flex' },
-                                      fontFamily: 'monospace',
-                                      fontWeight: 700,
-                                      letterSpacing: '.3rem',
-                                      color: 'inherit',
-                                      textDecoration: 'none',
-                                    }}
-                                  >
-                                    CookHub
-                                  </Typography>
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                          mr: 2,
+                          display: { xs: 'none', md: 'flex' },
+                          fontFamily: 'monospace',
+                          fontWeight: 700,
+                          letterSpacing: '.3rem',
+                          color: 'inherit',
+                          textDecoration: 'none',
+                        }}
+                      >
+                        CookHub
+                      </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
